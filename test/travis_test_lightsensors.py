31b3603046d1fa8584423a0d091179ef73f0e190
Yoshiaki Katada <yoshiaki_katada@yahoo.co.jp>
2019-08-29 03:25:14 +0000

    Change __init__ to setUp

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -5,7 +5,7 @@ import time
 from pimouse_ros.msg import LightSensorValues
 
 class LightsensorTest(unittest.TestCase):
-    def __init__(self):
+    def setUp(self):
         self.count = 0
         rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
         self.values = LightSensorValues()
